1e5821c1fca6386620f1e42330bb2e02dd72ec4e
Cameron <ramsellcameron@gmail.com>
2020-06-16 17:42:46 +0100

    fix

--- src/main/java/co/turtlegames/core/util/TRegion.java
+++ src/main/java/co/turtlegames/core/util/TRegion.java
@@ -22,11 +22,13 @@ public class TRegion {
         _firstPosition = firstPosition;
         _secondPosition = secondPosition;
 
-        _firstPosition = new Vector(Math.min(_firstPosition.getBlockX(), _secondPosition.getBlockX()),
+        _firstPosition = new Vector(
+                Math.min(_firstPosition.getBlockX(), _secondPosition.getBlockX()),
                 Math.min(_firstPosition.getBlockY(), _secondPosition.getBlockY()),
                 Math.min(_firstPosition.getBlockZ(), _secondPosition.getBlockZ()));
 
-        _secondPosition = new Vector(Math.max(_firstPosition.getBlockX(), _secondPosition.getBlockX()),
+        _secondPosition = new Vector(
+                Math.max(_firstPosition.getBlockX(), _secondPosition.getBlockX()),
                 Math.max(_firstPosition.getBlockY(), _secondPosition.getBlockY()),
                 Math.max(_firstPosition.getBlockZ(), _secondPosition.getBlockZ()));
 
@@ -61,14 +63,14 @@ public class TRegion {
     }
 
     public Vector getDimensions() {
-        return _secondPosition.clone().subtract(_firstPosition);
+        return _secondPosition.clone().add(new Vector(1,1,1)).subtract(_firstPosition);
     }
 
     public boolean contains(Block block) {
 
         boolean isInX = block.getX() >= _firstPosition.getBlockX() && block.getX() <= _secondPosition.getBlockX();
-        boolean isInY = block.getY() >= _firstPosition.getBlockX() && block.getY() <= _secondPosition.getBlockY();
-        boolean isInZ = block.getZ() >= _firstPosition.getBlockX() && block.getZ() <= _secondPosition.getBlockZ();
+        boolean isInY = block.getY() >= _firstPosition.getBlockY() && block.getY() <= _secondPosition.getBlockY();
+        boolean isInZ = block.getZ() >= _firstPosition.getBlockZ() && block.getZ() <= _secondPosition.getBlockZ();
 
         return isInX && isInY && isInZ;
 
